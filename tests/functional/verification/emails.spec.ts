adc071d449e0295e4560cf66c371b8e48bee5252
Makovsky Dmitry <dima@makovsky.me>
2024-11-12 18:44:18 +0300

    Add test for resending email verification link

--- tests/functional/verification/emails.spec.ts
+++ tests/functional/verification/emails.spec.ts
@@ -4,6 +4,7 @@ import testUtils from '@adonisjs/core/services/test_utils'
 import mail from '@adonisjs/mail/services/main'
 import VerifyEmailNotification from '#mails/verify_email_notification'
 import env from '#start/env'
+import Account from '#models/account'
 
 test.group('Verification Emails', (group) => {
   group.each.setup(() => testUtils.db().withGlobalTransaction())
@@ -21,6 +22,20 @@ test.group('Verification Emails', (group) => {
     email?.message.assertTo(testAccount.email)
     email?.message.assertFrom(env.get('MAIL_FROM'))
     email?.message.assertSubject('Verify your account')
+    cleanup(() => {
+      mail.restore()
+    })
+  })
+  test('Resend email verification link', async ({ client, route, cleanup }) => {
+    const { mails } = mail.fake()
+    await client.post(route('auth.register')).json(testAccount)
+    mails.assertQueuedCount(VerifyEmailNotification, 1)
+
+    const account = await Account.findByOrFail('email', testAccount.email)
+    await client.post(route('verification.email.send')).loginAs(account)
+
+    mails.assertQueuedCount(VerifyEmailNotification, 2)
+
     cleanup(() => {
       mail.restore()
     })
